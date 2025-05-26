fa42c09ed4eda53f290a2489d0b78551f883b342
koushik0623 <chenchukoushik@gmail.com>
2025-05-26 15:46:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openForm() {
+    document.getElementById("quoteForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("quoteForm").style.display = "none";
+  }
+  
